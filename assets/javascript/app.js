f6fdadd91df4dcda72c28a712e708eb4cb98b67f
Manuel Camilo <mcamilo@protonmail.com>
2019-10-02 19:07:37 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,15 @@
-// set a timer for each question
-$(document).ready(function(){
-   $("button").click(function(){
-      $("#startGameBtn").toggle();
-    });
-  });
-
-// var for userScore
+// var for userScore and counter
 var userScore = 0;
+var counter = 300; // it's 300 seconds, I'm going to display it as minutes and seconds
+
+// set a timer for each question
+setInterval("timer()", 1000)
+
+function timer () {
+  counter--;
+
+  $("#counter").text(counter);
+}
 
 // an object that has all my questions
 
