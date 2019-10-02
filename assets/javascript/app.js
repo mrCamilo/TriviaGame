f6d902b890dce7d6fa520d52a407fa9ec7db19ce
Manuel Camilo <mcamilo@protonmail.com>
2019-10-02 14:56:41 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,13 +11,13 @@ var userScore = 0;
 // an object that has all my questions
 
 // When you click the #startGameButton, ask the first question
-//$(document).ready(function () {
-  //  $("#startGameBtn").click(function(){
-    //    $(".centralContainer").html(
-      //      "<p>Question 1 blah blah blah blah blah blah ??</p>");
-    //}
-    //);
-//});
+$(document).ready(function () {
+    $("#startGameBtn").click(function(){
+        $(".centralContainer").html(
+            "<p>Question 1 blah blah blah blah blah blah ??</p>");
+    }
+    );
+});
 
 // Get rid of the start button when you click it
 $("#startGameBtn").click(function(){
@@ -32,6 +32,3 @@ $(document).ready(function () {
     }
     );
 });
-
-
-// Create 4 buttons for multiple choice answers (bootstrap)
