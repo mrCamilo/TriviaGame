9a6569468f9c25ffd57e235f58f8e6ee5a6385cf
Manuel Camilo <mcamilo@protonmail.com>
2019-10-02 10:09:14 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,9 @@
-
-// display question and 4 buttons (each a multiple choice answer)
-//function askAQuestion(){
-    //document.getElementById("firstQuestion").innerHTML = "What is the only episode of Spongebob where Larry the Lobster is referred to as Big Larry?" + console.log("hey it me"); 
-//}
-
 // When you click the #startGameButton, ask the first question
 $(document).ready(function () {
     $("#startGameBtn").click(function(){
-        $(".centralContainer").html("drain gangg!");
+        $(".centralContainer").html("<p>Question 1 blah blah blah blah blah blah ??</p> <h3>oh yeah yeah</h3>");
     }
     );
 });
 
+// Create 4 buttons for multiple choice answers
