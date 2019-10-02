8052bd15bdc4fb03f9f92fbc33fbe0025fc7c6c8
Manuel Camilo <mcamilo@protonmail.com>
2019-10-01 21:08:41 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,11 @@
 // function to initialize the game
 // call this AFTER instructions are given out and user clicks a button
+$(document).ready(function () {
+    $("#startGameBtn").on("click", function () {
+        $("#alignment").html("");
+    });
+});
+
+function startGame(){
+
+}
