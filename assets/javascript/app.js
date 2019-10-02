f8b38dcac02c66a51bc55e6a379c6149db78a5c3
Manuel Camilo <mcamilo@protonmail.com>
2019-10-02 11:36:07 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
+// an object that has all my questions
+var num1 = "ohhhh ohhh ohh ohh oh hhh";
+
 // When you click the #startGameButton, ask the first question
 $(document).ready(function () {
     $("#startGameBtn").click(function(){
-        $(".centralContainer").html("<p>Question 1 blah blah blah blah blah blah ??</p> <h3>oh yeah yeah</h3>");
+        $(".centralContainer").html(num1 + "<p>Question 1 blah blah blah blah blah blah ??</p> <h3>oh yeah yeah</h3>");
     }
     );
 });
