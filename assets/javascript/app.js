4b0f4a96e62b3e6d28618b83a60c5002357e1e5a
Manuel Camilo <mcamilo@protonmail.com>
2019-10-03 13:44:58 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,41 +16,44 @@ function timer () {
 // an object that has all my questions
 
 // Get rid of the start button when you click it
-$("#startGameBtn").click(function(){
-    $("#startGameDiv").empty();
-  });
+//$("#startGameBtn").click(function(){
+    //$("#startGameDiv").empty();
+  //});
 
 // toggle the first set of questions
 $("#startGameBtn").click(function(){
+    $("#startGameDiv").empty();
     $(".firstSetOfQuestions").toggle(
         "#fourQuestionButtons");
 });
 
 // empty it again?
-$(".btn-secondary").click(function(){
-  $(".firstSetOfQuestions").empty();
-});
+//$(".btn-secondary").click(function(){
+  //$(".firstSetOfQuestions").empty();
+//});
 
 // the second set of questions
 $(".btn-secondary").click(function(){
+  $(".firstSetOfQuestions").empty();
   $(".secondSetOfQuestions").toggle(
       "#fourMoreQuestionButtons");
 });
 
 // empty it again
-$(".thirdButton").click(function(){
-  $(".secondSetOfQuestions").empty();
-});
+//$(".thirdButton").click(function(){
+  //$(".secondSetOfQuestions").empty();
+//});
 
 // last set of questions
 $(".thirdButton").click(function(){
+  $(".secondSetOfQuestions").empty();
   $(".lastSetOfQuestions").toggle(
       "#fourLastQuestionButtons");
 });
 
 // Get rid of the buttons after you click
-$(".lastButton").click(function(){
-  $(".lastSetOfQuestions").empty();
-});
+//$(".lastButton").click(function(){
+  //$(".lastSetOfQuestions").empty();
+//});
 
 //move on to the results
