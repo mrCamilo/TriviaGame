84e0b3bac4fb468add9c7ea92dc4708f5585f86c
Manuel Camilo <mcamilo@protonmail.com>
2019-10-04 09:23:04 -0400

    .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,33 +13,31 @@ function timer () {
 // an object that has all my questions
 
 $("#startGameBtn").click(function(){
+  userScore++;
     $("#startGameDiv").empty(); // Get rid of the start button when you click it
     $(".firstSetOfQuestions").toggle( // toggle the first set of questions
-        "#fourQuestionButtons");
-        
-        userScore++;
+        "#fourQuestionButtons");      
 });
 
 // the second set of questions
 $(".btn-secondary").click(function(){
   $(".firstSetOfQuestions").empty(); // empty it again?
   counter = 100;
+  userScore+=1;
   timer(); // reset the timer
   
   $(".secondSetOfQuestions").toggle( // toggle the next set of questions
       "#fourMoreQuestionButtons");      
-      userScore+=1;
 });
 
 // last set of questions
 $(".thirdButton").click(function(){
   $(".secondSetOfQuestions").empty(); // empty it again
+  userScore+=1; // increment user score
   counter = 100;
   timer(); // reset the timer
   $(".lastSetOfQuestions").toggle( // toggle the last set of questions
       "#fourLastQuestionButtons");
-      
-      userScore+=1; // increment user score
 });
 
 // Get rid of the buttons after you click, make way for results screen
